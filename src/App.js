5ccc5408fcda92de39e07bd8fe1b67cf8ae87aa1
Yash Pratap Singh <singhyashpratap06@gmail.com>
2020-07-29 23:14:57 +0530

    TYPE:FEATURE -> create basic skeloton for main navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import Header from "./components/Header/Header";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Future Netflix App</h1>
+    <div>
+      <Header currentRoute={this.props.location.pathname} />
+      {/* <Switch>
+        <Route exact path="/" component={Movies} />
+        <Route path="/movies/:title" component={MovieItemPage} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/tvshows/:name" component={TVShowItemPage} />
+        <Route path="/tvshows" component={TvShow} />
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            this.props.currentUser ? <Redirect to="/" /> : <SignIn />
+          }
+        />
+        <Route
+          exact
+          path="/signup"
+          render={() =>
+            this.props.currentUser ? <Redirect to="/" /> : <SignUp />
+          }
+        />
+        <Route exact path="/searchresults" component={SearchPage} />
+        <Route exact path="/mylist" component={ListOverview} />
+      </Switch> */}
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,79 @@
+import React , { Component }from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../Assets/logo.png";
+import "./Header.scss";
+import { withRouter } from "react-router";
+import SearchBar from "../SearchBar/SearchBar";
+
+
+
+
+
+class Header extends Component {
+
+    render() {
+        return (
+            <div className="header">
+                <div className="header__logo-box" onClick={() => this.props.history.push("/movies")}>
+                    <img src={Logo} alt="logo" className="header__logo" />
+                </div>
+
+                <div className="header__options">
+                    <div className="header__options-primary">
+                        <Link className="header__option" to="/movies">
+                            Movies
+                  </Link>
+                        <Link className="header__option" to="/tvshows">
+                            TV Shows
+                  </Link>
+
+                        <Link className="header__option" to="/mylist">
+                            My List
+                  </Link>
+                    </div>
+
+                    <div className="header__searchbar">
+                        <SearchBar currentRoute={this.props.currentRoute} />
+                    </div>
+
+                    {this.props.currentUser ? (
+                        <div className="header__options-secondary">
+                            <Link className="header__option" to="">
+                                Hi, {this.props.currentUser.displayName}
+                            </Link>
+                            <div
+                                className="header__option header__option--signout"
+                                onClick={() => auth.signOut()}
+                            >
+                                Sign Out
+                    </div>
+                        </div>
+                    ) : (
+                            <div className="header__options-secondary">
+                                <Link className="header__option" to="">
+                                    Hi, Guest
+                    </Link>
+                                <Link
+                                    className="header__option header__option--signin"
+                                    to="/signin"
+                                >
+                                    Sign In
+                    </Link>
+                            </div>
+                        )}
+                </div>
+                <FontAwesomeIcon
+                    icon={faBars}
+                    className="header__nav-menu-icon"
+                    onClick={this.props.ToggleMenuHidden}
+                />
+                {this.props.hidden ? null : <NavMenu />}
+            </div>
+        );
+    }
+}
+
+
+
+export default Header;
+
